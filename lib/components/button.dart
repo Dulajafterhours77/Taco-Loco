f433fd3eadd166da1a90b2a3e3bc305d594ff424
Risini Amarathunga <162025795+Rizz-33@users.noreply.github.com>
2024-03-08 21:28:05 +0530

    Update button.dart

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -19,29 +19,29 @@ class Button extends StatelessWidget {
         decoration: BoxDecoration(
           gradient: LinearGradient(
             colors: [primaryColor, Color.fromRGBO(
-                    primaryColor.red, primaryColor.green, primaryColor.blue, 0.76),],
+                    primaryColor.red, primaryColor.green, primaryColor.blue, 0.75),],
             begin: Alignment.centerLeft,
             end: Alignment.centerRight,
           ),
           borderRadius: BorderRadius.circular(10),
         ),
-        padding: EdgeInsets.all(15.0),
+        padding: const EdgeInsets.all(15.0),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             //text
             Text(
               text,
-              style: TextStyle(
+              style: const TextStyle(
                 color: Colors.white,
                 fontSize: 20,
               ),
             ),
 
-            SizedBox(width: 20,),
+            const SizedBox(width: 20,),
 
             //icon
-            Icon(Icons.arrow_forward_ios, color: Colors.white, size: 20,)
+            const Icon(Icons.arrow_forward_ios, color: Colors.white, size: 20,)
           ],
         ),
       ),
